40eedaf331a59cadff7af04be85acbd50f241abe
Abhishek Tomar <ohyesabhiabhi@outlook.com>
2019-07-24 00:08:35 +0530

    - Inside Pattern Matrix problem

---
+++ DataStructureArray/MultiDiminsional_Array/Inside_Pattern.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataStructureArray.MultiDiminsional_Array
+{
+    class Inside_Pattern
+    {
+        static void Main(string[] args)
+        {
+            int n = Convert.ToInt32(Console.ReadLine());
+            int size = 2 * n - 1;
+            int front = 0;
+            int end = size - 1;
+            int[,] arr = new int[size, size];
+            while (n > 0)
+            {
+                for (int i = front; i <= end; i++)
+                {
+                    for (int j = front; j <= end; j++)
+                    {
+                        if (i == front || i == end || j == front || j == end)
+                        {
+                            arr[i, j] = n;
+                        }
+                    }
+                }
+                front++;
+                end--;
+                n--;
+            }
+            print(arr, size);
+            Console.ReadLine();
+        }
+        static void print(int[,] arr, int n)
+        {
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < n; j++)
+                    Console.Write(arr[i, j]);
+                Console.WriteLine();
+            }
+        }
+    }
+}
